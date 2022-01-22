7230f0e94319820adc8dbfca1962c6a99687bfe1
jsusilo <enr@bk.ru>
2022-01-23 00:01:46 +0500

    home handle

---
+++ src/components/Home/HandleAny.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class HandleAny extends React.Component {
+  render() {
+    return (
+      <section className="section section-md bg-default">
+        <div className="container wow fadeInRight">
+          <h2 className="text-color1">
+            {" "}
+            We'll handle any
+            <br />
+            <span className="font-weight-light">intricate custom design</span>
+          </h2>
+          <div className="row">
+            <div className="col-lg-6">
+              <p>
+                Steel and Fabrication Industry takes great pride in our
+                industry-leading custom metal fabrication services. This is the
+                core of our business and it’s a process we take seriously. We
+                work on small, large, and heavy components. Our team of experts
+                provides over a dozen metal fabrication services. We work with a
+                large variety of metals and raw material forms. Our loyal
+                customers come to us from a diverse array of industries. Their
+                applications and products are numerous, which include sheet
+                metal, complex assemblies, music equipment, fuel tanks,
+                fixtures, and much more.
+              </p>
+            </div>
+            <div className="col-lg-6">
+              <p>
+                Steel and Fabrication Industry offers the most cost-effective,
+                high-quality engineering and design services around. With our
+                consulting, designing, prototyping, and other expert services,
+                we consistently save customers time and money. We also offer a
+                host of additional services, all under the initiative of
+                environmentally-friendly, superior-quality work.
+              </p>
+              <a className="btn" href="#">
+                read more
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default HandleAny;
